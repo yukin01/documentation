525658007e3388d9f613dfc0df361da11683dc02
david.jones <david.jones@datadoghq.com>
2022-11-10 12:45:04 +0000

    updates

--- local/bin/py/build/actions/format_link.py
+++ local/bin/py/build/actions/format_link.py
@@ -45,7 +45,7 @@ if os.getenv("CI_COMMIT_REF_NAME"):
 class Node:
     # TODO: cleanup interactions and instance variables
     __slots__ = ['name', 'children', 'parent', 'lines', 'modified_lines', 'start_line', 'end_line', 'start', 'end',
-                 'ignore']
+                 'ignore', 'is_closing_shortcode']
 
     def __init__(self, name, ignore=False):
         self.name = name
@@ -58,6 +58,7 @@ class Node:
         self.start = 0
         self.end = 0
         self.ignore = ignore
+        self.is_closing_shortcode = False
 
     def add(self, child):
         child.parent = self
@@ -87,21 +88,9 @@ def parse_file(file):
     root = Node("root")
     current_node = root
 
-    open_tag_regex = r"{{[<|%]\s+([A-Za-z0-9-_]+)(.*)\s+[%|>]}}"
+    open_tag_regex = r"{{[<|%]\s+([A-Za-z0-9-_]+)(.*)\s*[%|>]}}"
     closed_tag_regex = r"{{[<|%]\s+/([A-Za-z0-9-_]+)(.*)\s*[%|>]}}"
     ignore_shortcodes = ('code-block', )
-
-    # list of tags that don't have open/close and are just one liner
-    # TODO: this isn't sustainable, we need to detect one liners or specify includes not excludes
-    one_liner_tags = ("partial", "img", "region-param", "latest-lambda-layer-version", "X", "dashboards-widgets-api",
-                      "api-scopes", "vimeo", "integrations", "jqmath-vanilla", "translate", "get-service-checks-from-git",
-                      "get-metrics-from-git", "appsec-getstarted", "appsec-getstarted-2", "appsec-getstarted-2-canary",
-                      "sdk-version", "notifications-integrations", "notifications-email", "get-npm-integrations",
-                      "permissions", "aws-permissions" "dbm-sqlserver-before-you-begin", "log-libraries-table",
-                      "serverless-libraries-table", "tracing-libraries-table", "classic-libraries-table",
-                      "dbm-sqlserver-agent-setup-kubernetes", "dbm-sqlserver-agent-setup-docker",
-                      "dbm-sqlserver-agent-setup-linux", "dbm-sqlserver-agent-setup-windows",
-                      "managed-locations")
 
     with open(file, 'r', encoding='utf-8') as f:
         new_line_number = 0
@@ -112,15 +101,16 @@ def parse_file(file):
             new_node = None
 
             # is this an opening triple backtick code block
-            if line.startswith("```") and current_node.name != "```":
+            tickmatches = list(re.finditer("^\s*```", line, re.MULTILINE))
+            if tickmatches and current_node.name != "```":
                 current_node.start = 0
                 new_node = Node("```", True)
 
             # find new open shortcode tags and create a node
-            matches = re.finditer(open_tag_regex, line, re.MULTILINE)
-            for matchNum, match in enumerate(matches, start=1):
-                tag_name, current_node.start = match.group(1), match.start(0)
-                if tag_name not in one_liner_tags:
+            if not current_node.ignore:
+                matches = re.finditer(open_tag_regex, line, re.MULTILINE)
+                for matchNum, match in enumerate(matches, start=1):
+                    tag_name, current_node.start, current_node.end = match.group(1), match.start(0), match.end()
                     new_node = Node(tag_name, tag_name in ignore_shortcodes)
 
             # if we entered a new node lets set it as the current
@@ -132,7 +122,8 @@ def parse_file(file):
                 new_line_number = 0
 
             # is this a closing triple backtick code block
-            if current_node.name == "```" and line.startswith("```") and not new_node:
+            if current_node.name == "```" and tickmatches and not new_node:
+                current_node.is_closing_shortcode = True
                 current_node.end = 0
                 current_node.end_line = current_node.start_line + 1
                 new_line_number = current_node.end_line
@@ -144,6 +135,7 @@ def parse_file(file):
             for matchNum, match in enumerate(matches, start=1):
                 tag_name, current_node.end = match.group(1), match.end(0)
                 if tag_name == current_node.name and current_node.parent:
+                    current_node.is_closing_shortcode = True
                     # if we closed on the same line we don't want to add the line again and end_line is the same
                     is_same_line = new_line_number == 0
                     current_node.end_line = current_node.start_line if is_same_line else current_node.start_line + 1
@@ -154,10 +146,37 @@ def parse_file(file):
 
             new_line_number += 1
 
+    # any node that is false still is a one liner
+    adjust_one_liner_shortcodes(root)
+
     if not root.lines:
         raise ValueError
 
     return root
+
+
+def adjust_one_liner_shortcodes(node):
+    """
+    As we can't find a closing tag for a one liner everything becomes nested under this shortcode
+    which isn't accurate. So lets readjust once we know
+    @param node: node
+    """
+    for i, n in enumerate(node.children):
+        if not n.is_closing_shortcode:
+            n.end_line = n.start_line
+            # take nodes children and put it adjacent to node
+            r, node.children[i].children[:] = n.children[:], []
+            node.children += r
+            for d in r:
+                # adjust child line offsets
+                d.start_line = d.start_line + n.start_line
+                d.end_line = d.end_line + n.start_line
+                # re-parent
+                d.parent = node
+            # move lines of text to parent
+            foo, node.children[i].lines = n.lines[1:], n.lines[0:1]
+            node.lines += foo
+        adjust_one_liner_shortcodes(n)
 
 
 def process_nodes(node):
@@ -166,7 +185,10 @@ def process_nodes(node):
     Takes the parsed node structure and processes the link formatting we desire throughout each node.
     @param node: node
     """
-    if not node.ignore:
+    if node.ignore or not node.is_closing_shortcode:
+        # ignored nodes we still need to return its original text or its removed
+        node.modified_lines = node.lines
+    else:
         content = ''.join(node.lines)
 
         # extract footer reference links
@@ -261,9 +283,6 @@ def process_nodes(node):
         if end_line == 0:
             end_line = start_line + 1
         node.modified_lines[start_line:end_line] = [f"[{i}]: {link}\n" for i, link in inline_refs.items()]
-    else:
-        # ignored nodes we still need to return its original text or its removed
-        node.modified_lines = node.lines
 
     # process children
     for child in node.children:
@@ -284,13 +303,18 @@ def assemble_nodes(node):
         if child.start_line == child.end_line:
             # single line shortcode
             line = output[child.start_line]
-            if child_output:
-                output[child.start_line] = line[:child.start] + child_output + line[child.end:]
+            if child.is_closing_shortcode:
+                if child_output:
+                    output[child.start_line] = line[:child.start] + child_output + line[child.end:]
+                else:
+                    # lets just use the full line for now
+                    # TODO: rebuild from char start to char end like above for interspersed shortcode amongst text
+                    #       e.g something like line[:child.start] + line[child.end:] should work but doesn't
+                    output[child.start_line] = line
             else:
-                # lets just use the full line for now
-                # TODO: rebuild from char start to char end like above for interspersed shortcode amongst text
-                #       e.g something like line[:child.start] + line[child.end:] should work but doesn't
+                #output[child.start_line] = line[child.start:child.end]
                 output[child.start_line] = line
+
         else:
             # multi line shortcode
             output[child.start_line:child.end_line + 1] = child_output
